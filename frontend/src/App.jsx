7c62ac6762c5b84cb24ac3d87702bc0f5196dae5
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2024-09-20 16:05:11 +0800

    Worked on the Navbar component
    - did basic setup of the pages
    - created navbar component with drawer in it
    - created MainLayout layout that holds navbar currently
    - created HomePage for the first page
    - did some basic routing for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' >
+      <Route path='/' element={<MainLayout />} >
         <Route index element={<HomePage />} />
       </Route>
     )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,69 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="navbar bg-primary text-primary-content w-full">
+            <div className="flex-none">
+              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </label>
+            </div>
+            <div className="mx-2 flex-1 px-2">Navbar Title</div>
+            <div className="flex-none lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                <li>
+                  <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                      </svg>
+                      <span className="badge badge-xs badge-error indicator-item">1</span>
+                    </div>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* Page content here */}
+          Content
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li><strong class="text-3xl">Hi Ryan</strong></li>
+            <hr></hr>
+            <li><a className="active">View Schedule</a></li>
+            <li><a>Manage My Applications</a></li>
+            <li><a>Manage Other's Applications</a></li>
+          </ul>
+        </div>
+      </div>
+    
+    </>
   )
 }
 
