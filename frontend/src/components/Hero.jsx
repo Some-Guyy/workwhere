747a147765030bfaf92076c2bc280cbd24026985
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2024-09-22 00:04:25 +0800

    Update Hero
    - Minor bug fixxes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,10 +4,10 @@ import Stats from "./Stats";
 const Hero = () => {
   return (
     <div className="mt-20">
-        <div className="hero bg-primary min-h-screen glass">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left lg:ml-5  text-base-100">
-                    <div className="glass p-4">
+        <div className="container bg-primary min-h-screen glass w-full">
+            <div className="flex flex-col lg:flex-row-reverse items-center">
+                <div className="text-center lg:text-left lg:ml-5  text-base-100 w-full p-4">
+                    <div className="glass p-4 w-full">
                         <h1 className="text-5xl font-bold">Hi Ryan!</h1>
                         <p className="py-6">
                             You have 2 more days of WFH this month, your next WFH is on 2nd October 2024. You  have 10 requests not approved yet.
