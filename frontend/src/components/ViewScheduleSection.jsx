0d4dcd6b5ddef3d673d39226c85a5de3998211dd
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2024-09-22 12:45:25 +0800

    Reworked logic and broke down components in Your Schedule Section
    - Broke down filter buttons in your schedule section to have its own component: FilterButtonsSection
    
    - Broke down Accordion to accordion row components
    
    - reworked Spinner component to fit new logic
    
    - done on 22/09/2024 12.45pm

--- frontend/src/components/ViewScheduleSection.jsx
+++ frontend/src/components/ViewScheduleSection.jsx
@@ -1,5 +1,6 @@
 import Accordion from "./Accordion";
 import { useEffect, useState } from "react";
+import FilterButtonsSection from "./FilterButtonsSection";
 
 const ViewScheduleSection = () => {
 
@@ -27,38 +28,18 @@ const ViewScheduleSection = () => {
         <div className="flex">
             <div className="basis-1/5 "></div>
             <div className="basis-3/5 my-5">
-                <div className="text-center mb-5">
-                    <button onClick={ () => {
-                        resetAllStates(); 
-                        setYourSchedule(true);
-                        setActiveSchedule("Your Schedule");
-                        }} className ={yourSchedule ? "btn btn-outline btn-primary m-2 btn-active" : "btn btn-outline btn-primary m-2"}>
-                        Your Schedule
-                    </button>
-
-                    <button onClick={ () => {
-                        resetAllStates(); 
-                        setYourTeamSchedule(true);
-                        setActiveSchedule("Your Team Schedule");
-                        }} className ={yourTeamSchedule ? "btn btn-outline btn-primary m-2 btn-active" : "btn btn-outline btn-primary m-2"}>
-                        Your Team Schedule
-                    </button>
-
-                    <button onClick={ () => {
-                        resetAllStates(); 
-                        setYourDepartmentSchedule(true);
-                        setActiveSchedule("Your Department Schedule");
-                        }} className ={yourDepartmentSchedule ? "btn btn-outline btn-primary m-2 btn-active" : "btn btn-outline btn-primary m-2"}>
-                        Your Department Schedule
-                    </button>
-
-                    <button onClick={ () => {
-                        resetAllStates(); 
-                        setYourOverallSchedule(true);                        setActiveSchedule("Your Overall Schedule");
-                        }} className ={yourOverallSchedule ? "btn btn-outline btn-primary m-2 btn-active" : "btn btn-outline btn-primary m-2"}>
-                        Your Overall Schedule
-                    </button>
-                </div>
+                <FilterButtonsSection 
+                setYourSchedule={setYourSchedule}
+                setYourTeamSchedule={setYourTeamSchedule}
+                setYourDepartmentSchedule={setYourDepartmentSchedule}
+                setYourOverallSchedule={setYourOverallSchedule}
+                resetAllStates={resetAllStates}
+                setActiveSchedule={setActiveSchedule}
+                yourSchedule={yourSchedule}
+                yourTeamSchedule={yourTeamSchedule}
+                yourDepartmentSchedule={yourDepartmentSchedule}
+                yourOverallSchedule={yourOverallSchedule}
+                />
                 <Accordion />
             </div>
 
