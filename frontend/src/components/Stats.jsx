9288e4e80e9932c2a067f8a5d63c95841c967feb
Ryan2409 <ryan.ng.2022@scis.smu.edu.sg>
2024-09-24 02:46:10 +0800

    Added colour for stats and calander to sync
    - Added colour to sync calander with stats component

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -28,9 +28,9 @@ const Stats = ({loading, data}) => {
                 <div className="stat">
                     <div className="stat-title">Total WFH days taken</div>
                     <div className="stat-value text-primary">{loading ? <Spinner loading={loading} /> : (
-                        <>
+                        <div className="text-green-500">
                             {noOfWfhDays} days
-                        </>
+                        </div>
                     )} 
                     </div>
                     <div className="stat-desc mt-1">Next WFH day on 1 Jan 2024</div>
@@ -39,9 +39,9 @@ const Stats = ({loading, data}) => {
                 <div className="stat">
                     <div className="stat-title">Total leaves taken</div>
                     <div className="stat-value text-secondary">{loading ? <Spinner loading={loading} /> : (
-                        <>
+                        <div className="text-blue-600">
                             {noOfLeaves} days
-                        </>
+                        </div>
                     )} 
                     </div>
                     <div className="stat-desc mt-1">Next leave on 2 Jan 2024</div>
@@ -50,12 +50,12 @@ const Stats = ({loading, data}) => {
                 <div className="stat">
                     <div className="stat-title">Total pending requests</div>
                     <div className="stat-value">{loading ? <Spinner loading={loading} /> : (
-                        <>
+                        <div className="text-yellow-400 text-center">
                             {noOfPendingReq} 
-                        </>
+                        </div>
                     )} 
                     </div>
-                    <div className="stat-desc mt-1">Requests</div>
+                    <div className="stat-desc mt-1 text-center">Requests</div>
                 </div>
             </div>
         
