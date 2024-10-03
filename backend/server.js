38e7f0096ef7e290395dea44f0b0d19cafcce5cb
brandonlhk <brandon.lim.2022@scis.smu.edu.sg>
2024-10-03 17:08:46 +0800

    Sprint2: Login and Create working arrangements
    - untested login feature
    - untested creation of working arrangements

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,19 @@ const fetchWorkingArrangementsInBatches = async (ids, startDate, endDate) => {
     return workingArrangements
 }
 
+//get all dates between start and end
+function getDatesBetween(startDate, endDate) {
+    const dates = []
+    let currentDate = new Date(startDate)
+
+    while (currentDate <= new Date(endDate)) {
+        dates.push(new Date(currentDate))
+        currentDate.setDate(currentDate.getDate() + 1)
+    }
+
+    return dates
+}
+
 //get specific employee for working arrangements (personal work schedule)
 app.get("/working-arrangements/:employeeid", async (req, res) => {
     try {
@@ -185,7 +198,121 @@ app.get("/working-arrangements/team/:employeeId/:date", async (req, res) => {
     }
 })
 
-app.get("/login")
+// login ( ** NOT TESTED ***)
+app.post('/login', async (req, res) => {
+    const { emailAddress, password } = req.body
+
+    try {
+        // find user from user db
+        const userSnapshot = await db.collection('users')
+            .where('email_address', '==', emailAddress)
+            .get()
+
+        if (userSnapshot.empty) {
+            return res.status(401).json({ error: 'Invalid username or password' })
+        }
+
+        let userData = ""
+        userSnapshot.forEach(doc => {
+            userData = doc.data()
+        });
+
+        // compare password, if wrong return error message
+        if (password !== userData.password) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // get staff details from employee db
+        const employeeSnapshot = await db.collection('mock_employee')
+            .where('Staff_ID', '==', userData.staff_id)
+            .get();
+
+        // if something mess up lul
+        if (employeeSnapshot.empty) {
+            return res.status(404).json({ error: 'Staff details not found' });
+        }
+
+        let staffDetails = ""
+        employeeSnapshot.forEach(doc => {
+            staffDetails = doc.data();
+        });
+
+        // return the data back
+        res.json({
+            message: 'Login successful',
+            user: {
+                staff_id: staffDetails.Staff_ID,
+                staff_fname: staffDetails.Staff_FName,
+                staff_lname: staffDetails.Staff_LName,
+                dept: staffDetails.Dept,
+                position: staffDetails.Position,
+                reporting_manager: staffDetails.Reporting_Manager,
+            },
+        });
+
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//create new working arrangement (*** NOT TESTED ***)
+app.post('/working-arrangements', async (req, res) => {
+    try {
+        const { staff_id, startDate, endDate, time, reason } = req.body;
+
+        // get staff name from db
+        const employeeSnapshot = await firestore.collection('mock_employee')
+            .where('staff_id', '==', staff_id)
+            .get();
+
+        if (employeeSnapshot.empty) {
+            return res.status(404).json({ error: 'Employee not found' });
+        }
+
+        let staffDetails = ""
+        employeeSnapshot.forEach(doc => {
+            staffDetails = doc.data()
+        })
+
+        // unpack into variable
+        const { staff_fname, staff_lname } = staffDetails
+
+        // get the dates between startdate and enddate
+        const dates = getDatesBetween(startDate, endDate);
+
+        // create a working arrangement for each date
+        const batch = firestore.batch();
+
+        dates.forEach(date => {
+            const newDocRef = firestore.collection('working_arrangements').doc()
+            batch.set(newDocRef, {
+                Staff_ID: staff_id,
+                Staff_FName: staff_fname,
+                Staff_LName: staff_lname,
+                time,
+                reason,
+                startDate: Timestamp.fromDate(date),
+                endDate: Timestamp.fromDate(date), 
+                requestCreated: Timestamp.now(),
+                status: 'pending',
+                approvedBy: '', 
+                Approved_Fname: '', 
+                Approved_Lname: ''
+            });
+        });
+
+        // commit operation to create all documents
+        await batch.commit()
+
+        res.json({ message: 'Request created successfully' });
+
+    } catch (error) {
+        console.error('Error creating your request', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 // catch rogue calls
 app.all("*", (req, res) => {
