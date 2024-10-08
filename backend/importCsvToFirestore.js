f020eed14c2f3c032ce0f7fc3c0b09f3af7ba29c
HanWeiOng <hanwei.ong.2022@scis.smu.edu.sg>
2024-10-08 16:02:01 +0800

    modified csv import to be a function

--- backend/importCsvToFirestore.js
+++ backend/importCsvToFirestore.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -11,15 +11,15 @@ admin.initializeApp({
 const db = admin.firestore();
 
 // Path to your CSV file
-const filePath = './employeenew.csv';
+// const filePath = './employeenew.csv';
 
 // Function to import CSV data into Firestore
-const importCsvToFirestore = () => {
+const importCsvToFirestore = (filePath,collection) => {
   const batch = db.batch();  // Create a batch to write multiple documents at once
   fs.createReadStream(filePath)
     .pipe(csv())
     .on('data', (row) => {
-      const docRef = db.collection('employee').doc();  // You can also specify an ID instead of using .doc()
+      const docRef = db.collection(collection).doc();  // You can also specify an ID instead of using .doc()
       batch.set(docRef, row);  // Add the row to Firestore
     })
     .on('end', async () => {
@@ -32,4 +32,6 @@ const importCsvToFirestore = () => {
 };
 
 // Run the import function
-importCsvToFirestore();
+importCsvToFirestore('./employeenew.csv','employee');
+// importCsvToFirestore('./mockemployee.csv','mock_employee')
+// importCsvToFirestore('./mockworkarrangement.csv','mock_working_arrangements')
