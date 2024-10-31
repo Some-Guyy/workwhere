ecb518a65e78c684d6a16aaf32e7dd65c71fbac8
Clement-Chong <jjchong.2022@smu.edu.sg>
2024-10-31 20:05:18 +0800

    skeleton code with no end points

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,18 +6,65 @@ const Navbar = () => {
   const [EmployeeRole,setEmployeeRole] = useState(null)
   const [EmployeePosition,setEmployeePosition] = useState(null)
   const [EmployeeName, setEmployeeName] = useState(null);
+  const [EmployeeId, setEmployeeId] = useState(null)
+  const [notificationData, setNotificationData] = useState(null)
   const navigate = useNavigate();
   const location = useLocation();
   const drawerCheckboxRef = useRef(null);
+  let unseenNotifications = [];
   
   // extract users role to conditionally render links
   useEffect(()=>{
+      // fetchNotificationData()
       const data = JSON.parse(localStorage.getItem('state'));
       // console.log(data)
       setEmployeeName(data.staffFirstName);
       setEmployeeRole(data.role)
       setEmployeePosition(data.Position)
+      setEmployeeId(data.staffId)
   },[])
+
+  useEffect(()=>{
+    if(notificationData!= null){
+      unseenNotifications = notificationData.filter(notifiction => notifiction.status == unseen)
+    }
+},[notificationData])
+
+
+
+  const fetchNotificationData = async () =>{
+    const apiUrl = ``;
+    try{
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      setNotificationData(data)
+    }
+    catch(error){
+      console.log("Error fetching notification data")
+    }
+
+  }
+
+  const updateNotifictionStatus = async (notificationId) => {
+    unseenNotifications = unseenNotifications.filter(notifiction => notifiction.Id != notificationId)
+    const apiUrl = ``
+    try{
+      const res = await fetch(apiUrl, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:"",
+      });
+    }
+    catch(error){
+      console.log("Error updating notification data")
+    }
+
+
+
+  }
+
 
   // Close the drawer when navigating to another page
   useEffect(() => {
@@ -60,10 +107,11 @@ const Navbar = () => {
                 Workwhere
               </span>
             </div>
-            <div className="flex-none lg:block">
+            <div className="flex lg:block">
               <ul className="menu menu-horizontal">
                 {/* Navbar menu content here */}
                 <li>
+                <div className="dropdown dropdown-bottom dropdown-end mr-4">
                   <button className="btn btn-ghost btn-circle animate-pulse">
                     <div className="indicator">
                       <svg
@@ -78,9 +126,63 @@ const Navbar = () => {
                           strokeWidth="2"
                           d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                       </svg>
-                      <span className="badge badge-xs badge-error indicator-item">1</span>
+                      <span className="badge badge-xs badge-error indicator-item"></span>
                     </div>
                   </button>
+                  <ul className="dropdown-content menu p-2 text-sm shadow bg-gray-600 rounded-box w-80 z-10 max-h-60 overflow-y-auto flex flex-col flex-nowrap">
+                    {notificationData != null ? 
+                      notificationData.map((l,k)=>{
+                        return <li key={l.Id}className="relative p-4 whitespace-normal hover:bg-gray-500" onMouseLeave={updateNotifictionStatus}>
+                          {l in unseenNotifications ? <span className="absolute right-0 badge badge-xs badge-error indicator-item mt-2 mr-2"></span>:null}
+                        </li>
+                      })
+                    
+                  :<li className="relative p-4 whitespace-normal">no notification</li>}
+                    <li className="relative p-4 whitespace-normal hover:bg-gray-500" onMouseLeave={updateNotifictionStatus}>
+                    <span className="absolute right-0 badge badge-xs badge-error indicator-item mt-2 mr-2"></span>
+                    your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-4 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-4 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+                    <li className=" p-2 whitespace-normal">
+                      your WFH arrangement on 24/10/2001 has been rejected reason:no
+                    </li>
+                    <div className="divider mt-0 mb-0"></div>
+
+
+                  </ul>
+                </div>
                 </li>
               </ul>
             </div>
