f9d634e7359032e396271921270d336ab8c794ae
Some Guy <owen.tan.2022@scis.smu.edu.sg>
2024-11-07 21:24:57 +0800

    tweak name of test case

--- backend/tests/unit/app.unit.test.js
+++ backend/tests/unit/app.unit.test.js
@@ -1795,7 +1795,7 @@ describe('PUT /withdraw', () => {
     expect(db.collection().doc).toHaveBeenCalledWith('mock-doc-id');
   })
 
-  test('Managing director withdraw their own working arrangement', async () => {
+  test('managing director withdraw their own working arrangement', async () => {
     // Mock Firestore get method to return a snapshot with a matching document
     const mockGet = db.collection().get;
     const mockDocRef = {
